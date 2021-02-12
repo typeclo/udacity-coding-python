4d73342cd6ef114ca648206507e7a5b8b564c07b
Clouie Tantuan Gonzalez <clouie.tantuan@hotmail.com>
2021-02-12 22:14:49 +0100

    Completes Adventure game

--- adventure_game.py
+++ adventure_game.py
@@ -1,16 +1,124 @@
+import time
+import math
+import random
 
-treasure_found = False
-state = [["a", "b"],
-         ["c", "d"]]
-current_state = (0, 0)
+dangers = ["bear", "tiger", "snake", "fox",
+           "boar", "lion", "Python", "Gruffalo"]
 
+chap = {
+    "home": ("Should I 'bring my bag' or 'leave it'? ", "bring my bag", "leave it"),
+    "no_bag_fork": ("Yeah, I don't need a bag anyway..." + \
+                    " Which road should I take, the 'adventurous'" +\
+                    " road or the 'easy' road?", "adventurous", "easy"),
+    "fork_road": ("Better safe than sorry. Which road should I take,"+\
+                  " the 'adventurous' road or the 'easy' road?", "easy", "adventurous"),
+    "dangerous_road": ("Oh no there's a " + random.choice(dangers) + ", I won't make it today", None, None),
+    "no_bag_bushes": ("I feel so adventurous! Wait..."+\
+                      " what's that? I hear a sound in the bushes, "+\
+                      "I wonder if I should 'check' or 'keep walking'?", "keep walking", "check"),
+    "bushes": ("Grandpa always said this was the best way to go."+\
+               " Wait... what's that? I hear a sound in the bushes,"+\
+               " I wonder if I should 'check' or 'keep walking'?", "check", "keep walking"),
+    "danger": ("Oh no, it's a " + random.choice(dangers) + "! "+
+               "I better hurry back home! I'll try again tomorrow.", None, None),
+    "no_bag_cave_ent": ("Looks like I found a big cave!"+\
+                        " Too bad I didn't bring my flashlight..."+\
+                        " I left it in my bag! I guess I'll just go home...", None, None),
+    "cave_ent": ("Looks like I found a big cave!"+\
+                 " Good thing I have a flashlight in my bag."+\
+                 " Should I 'go in' or 'explore outside'?", "go in", "explore outside"),
+    "cave": ("This cave is so scary... Should I 'explore'"+\
+             " or 'go back outside'?", 'explore', "go back outside"),
+    "deep_cave": ("Wow, this cave is beautiful!"+\
+                  " Too bad I couldn't find that treasure. "+\
+                  "It's getting late, I'll just head back home.", None, None),
+    "outside_cave": ("Wow, I'm exhausted."+\
+                     " That lonely tree by the cave entrance looks like a good place to rest,"+\
+                     " should I take a 'rest' or 'keep exploring'?", "rest", "keep exploring"),
+    "lonely_tree": ("I guess I'll just sit under this tree to rest... " +\
+                    "Wait what's that sign on the ground. Should I 'dig' " +\
+                    "or 'explore some more'?", "explore some more", "dig"),
+    "getting_dark": ("Exploring sure is fun, but it's getting late."+\
+                     " I'll have to look for that treasure again tomorrow.", None, None),
+    "treasure_pos": ("I found the treasure!", None, None)
+}
+
+is_treasure_found = False
+# Grid of positions, each position has a text and two options. Options can be text or None.
+map = [[chap["home"],          chap["fork_road"],       chap["bushes"],       chap["danger"],       None],
+       [chap["no_bag_fork"],   chap["dangerous_road"],  chap["cave_ent"],     chap["cave"],         chap["deep_cave"]],
+       [chap["no_bag_bushes"], chap["no_bag_cave_ent"], chap["outside_cave"], chap["lonely_tree"],  chap["getting_dark"]],
+       [chap["danger"],        None,                    chap["getting_dark"], chap["treasure_pos"], None]]
+current_position = (0, 0)
+treasure_position = (3, 3)
+
+# Prints, then waits 2 seconds.
 def print_dialog(dialog):
-    print(dialog) 
-    
-def get_next_state():
-    coordinate_x = current_state[0]
-    coordinate_y = current_state[1]
-    state[]
-    action = input()
+    print(dialog)
+    time.sleep(2)
 
-    
+# Returns state tuple at current_position.
+def get_current_state_from_position():
+    coordinate_x = current_position[0]
+    coordinate_y = current_position[1]
+    return map[coordinate_x][coordinate_y]
+
+# Prompts user with the state in map at current_position and 
+# returns the next position based on user's input.
+def get_next_position(current_state):
+    while True:
+        action = input(current_state[0] + "\n")
+        if action == current_state[1]:
+            return (current_position[0], current_position[1] + 1)
+        elif action == current_state[2]:
+            return (current_position[0] + 1, current_position[1])
+        else:
+            print_dialog("Huh. What was I doing again... Ah right..")
+
+# Main game
+def start_game():
+    global is_treasure_found
+    global current_position
+
+    # Opening scene
+    print_dialog("My grandpa told me that he hid some treasures in the forest nearby.")
+    print_dialog("Today, I'm going to look for that treasure.")
+
+    # loops over map using get_next_position, until is_treasure_found is True.
+    while not is_treasure_found:
+        current_state = get_current_state_from_position()
+
+        has_player_lost = current_state[1] == None and current_state[2] == None
+        if has_player_lost:
+            print_dialog(current_state[0])
+            break
+
+        current_position = get_next_position(current_state)
+
+        # Check if current_position equals treasure_position.
+        if current_position == treasure_position:
+            current_state = get_current_state_from_position()
+            is_treasure_found = True
+            print_dialog(current_state[0])
+            break
+
+    # End scene: Player won.
+    if is_treasure_found:
+        print_dialog('I finally found the treasure, Grandpa will be so proud of me!')
+    # End scene: Player lost.
+    else:
+        print_dialog("I'll get some rest, maybe I can try again in the morning.")
+
+start_game()
+
+while not is_treasure_found:
+    should_start_over = input("It's the next day, should I try looking for treasure again? 'yes' or 'no'\n")
+    if should_start_over == "yes":
+        current_position = (0, 0)
+        start_game()
+        continue
+    elif should_start_over == "no":
+        print_dialog("Nah, not today!\n")
+        break
+    else:
+        print_dialog("Huh. What was I doing again... Ah right..")
--- order_breakfast.py
+++ order_breakfast.py
@@ -1,6 +1,45 @@
-order = input("Please place your order. Would you like waffles or pancakes?\n")
-if order.lower() == "waffles":
-    print("Waffles it is!")
-elif order.lower() == "pancakes":
-    print("Pancakes it is!")
-    
+import time
+
+
+def print_pause(message_to_print):
+    print(message_to_print)
+    time.sleep(2)
+
+
+def valid_input(prompt, option1, option2):
+    while True:
+        response = input(prompt).lower()
+        if option1 in response:
+            return response
+        elif option2 in response:
+            return response
+        else:
+            print_pause("Sorry, I don't understand.")
+
+
+print_pause("Hello! I am Bob, the Breakfast Bot.")
+print_pause("Today we have two breakfasts available.")
+print_pause("The first is waffles with strawberries and whipped cream.")
+print_pause("The second is sweet potato pancakes with butter and syrup.")
+
+
+while True:
+
+    response = valid_input("Please place your order. "
+                           "Would you like waffles or pancakes?\n",
+                           "waffles", "pancakes")
+    if "waffles" in response:
+        print_pause("Waffles it is!")
+    elif "pancakes" in response:
+        print_pause("Pancakes it is!")
+
+    print_pause("Your order will be ready shortly.")
+
+    order_again = valid_input("Would you like to place another order? "
+                              "Please say 'yes' or 'no'.\n",
+                              "yes", "no")
+    if "no" in order_again:
+        print_pause("OK, goodbye!")
+    elif "yes" in order_again:
+        print_pause("Very good, I'm happy to take another order.")
+
